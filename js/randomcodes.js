f7199c0ad026abdd877f2233d1d4c82e8369b3c0
Loss Shirley <shirleyloss@LossdeMacBook-Pro.local>
2022-05-26 20:50:27 -0700

    Finished JS section for Inquire code comparing and fixed bug in Contact HTML.

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -23,7 +23,7 @@ function generateCode(){
 document.getElementById("codes").innerHTML = generateCode();
 
 //determine when to able or disable button
-function disableButton(){
+function disableButton(btnvalue){
     document.getElementById("submit").disabled = btnvalue; // able or disable button
     if (btnvalue == true) { //test if button is disabled or enabled
         //set button and label color translucent
@@ -33,6 +33,21 @@ function disableButton(){
         //set button and label with no transparency
         document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
         document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
+}
+
+//listen to user input code
+var codebox = document.getElementById("codeentered"); //get textbox
+codebox.addEventListener("input", evaluateCode); //listen to code entered in textbox
+
+//run function if detected user had entered a character in textbox
+function evaluateCode(){
+    getCode = document.getElementById("codeentered").value; //get character entered
+    var charset1 = getCode.trim(); //remove any hidden characters entered
+    var charset2 = code.trim(); //remove any hidden characters generated
+    //test if code entered matches the number of generated characters
+    if(charset1.length == charset2.length && charset1 == charset2) {
+        disableButton(false); //if match, run the function to enable button
     }
 }
 
