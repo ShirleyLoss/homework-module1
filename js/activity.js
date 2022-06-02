5df457c38c2178c2faef82d6234890303994aba1
Loss Shirley <shirleyloss@LossdeMacBook-Pro.local>
2022-06-02 00:22:20 -0700

    Optimized JS code for activities page.

--- js/activity.js
+++ js/activity.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
     $("#activitiesTable > thead > tr > td").addClass("notAvailable");
     $("#activitiesTable > tbody > tr > td:first-child").addClass("notAvailable");
     $("#activitiesTable").find("td").not(".notAvailable").addClass("available");
-    $(".available").not(".activitiesTableSelected").click(function(){
-        $(this).addClass("activitiesTableSelected");
-    });
-    $(".available .activitiesTableSelected").click(function(){
-        // console.log($(this).attr('class'));
-        $(this).removeClass("activitiesTableSelected");
-        console.log($(this).attr('class'));
+    $(".available").hover(function(){
+        $(this).css('cursor', 'pointer');
+    })
+    $(".available").click(function(){
+        if($(this).hasClass("activitiesTableSelected")){
+            $(this).removeClass("activitiesTableSelected");
+        } else {
+            $(this).addClass("activitiesTableSelected");
+        }
     });
 });
