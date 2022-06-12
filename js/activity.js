e16044a49935555e91accb9f3af2b0775ca4a351
Loss Shirley <shirleyloss@LossdeMacBook-Pro.local>
2022-06-12 15:27:51 -0700

    Adjust code for 'Not Available activities.

--- js/activity.js
+++ js/activity.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $("#activitiesTable > thead > tr > td").addClass("notAvailable");
     $("#activitiesTable > tbody > tr > td:first-child").addClass("notAvailable");
+    $("#activitiesTable > tbody > tr > td:contains('Not Available')").addClass("notAvailable");
     $("#activitiesTable").find("td").not(".notAvailable").addClass("available");
     $(".available").hover(function(){
         $(this).css('cursor', 'pointer');
