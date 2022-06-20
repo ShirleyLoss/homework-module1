34514a6e8823e3ca581e09e5f0513dcaee194cf6
Loss Shirley <shirleyloss@LossdeMacBook-Pro.local>
2022-06-19 20:43:25 -0700

    Updated files based on requirements for Week 7 Day 2 homework.

--- js/activity.js
+++ js/activity.js
@@ -10,15 +10,21 @@ $(document).ready(function(){
         var index = $(this).index()+1;
         $(this).toggleClass("activitiesTableSelected");
         if($(this).hasClass("activitiesTableSelected")) {
-            $("#displaySelected").css('visibility', 'visible'); //make display box visible
-            $("#displaySelected").css('margin-top', '2em'); //add spaces above display box
+            // $("#displaySelected").css('visibility', 'visible'); 
+            //make display box visible
+            // $("#displaySelected").css('margin-top', '2em'); 
+            //add spaces above display box
             $("#result").append('<p>'+$(this).text()+' at ' + $("#activitiesTableHead td:nth-child("+index+")").text() + '</p>'); //add child element with contents of cell
         } else { //if selected cell don't have class
             $("#result  p:contains("+$(this).text()+')').remove(); //remove child element
-            if($("#result").has("p").length == false) { //check if there are any child elements within parent
-                $("#displaySelected").css('visibility', 'hidden'); //make display box hidden
-                $("#displaySelected").css('margin-top', '0'); //remove spaces above display box
-            };
+            // if($("#result").has("p").length == false) { 
+                //check if there are any child elements within parent
+                // $("#displaySelected").css('visibility', 'hidden'); 
+                //make display box hidden
+                // $("#displaySelected").css('margin-top', '0'); 
+                //remove spaces above display box
+            // };
         };
+        $("#modalButton").trigger("click");
     });
 });
